bcc145f2f9d91fe0b838a1b9e8029fb38497d6dd
v1s1t0r999 <aditya.funs.11@gmail.com>
2022-01-18 10:54:02 +0530

    Create auth.py

---
+++ ShitDB/auth.py
@@ -0,0 +1,24 @@
+from github import *
+
+
+class Data:
+	def __init__(self):
+		self.repo=""
+		self.key = ""
+		self.author=""
+		self.github=""
+
+
+
+
+class Initialize:
+	def __init__(self,github_token:str,database_repo:str,author:tuple,branch:str = None):
+		data = Data()
+		data.github = Github(github_token)
+		data.key = github_token
+		data.author = InputGitAuthor(name=author[0],email=author[1])
+		data.repo = self.github.get_repo(f"{author[0]}/{database_repo}")
+		
+
+
+
