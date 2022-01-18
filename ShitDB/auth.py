be9b982ed9b8bfc42d2807b707ef20f22c57e427
v1s1t0r999 <aditya.funs.11@gmail.com>
2022-01-18 11:05:43 +0530

    Update auth.py

--- ShitDB/auth.py
+++ ShitDB/auth.py
@@ -7,17 +7,19 @@ class Data:
 		self.key = ""
 		self.author=""
 		self.github=""
+		self.branch = ""
 
 
 
 
-class Initialize:
+class ConnectDB:
 	def __init__(self,github_token:str,database_repo:str,author:tuple,branch:str = None):
 		data = Data()
 		data.github = Github(github_token)
 		data.key = github_token
 		data.author = InputGitAuthor(name=author[0],email=author[1])
 		data.repo = self.github.get_repo(f"{author[0]}/{database_repo}")
+		data.branch = branch
 		
 
 
